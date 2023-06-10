2e1cbbb607a64a1dd2a2c0a36c928bb608766a57
Taner Özer <tanerozer16@gmail.com>
2023-06-10 17:20:52 +0300

    Update imdbcek.py

--- imdbvericekme/imdbcek.py
+++ imdbvericekme/imdbcek.py
@@ -1,38 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-url="https://www.imdb.com/chart/top"
+url = "https://www.imdb.com/chart/top"
 
-response=requests.get(url)
+response = requests.get(url)
 
-htmlicerigi=response.content
+htmlicerigi = response.content
 
-soup=BeautifulSoup(htmlicerigi,"html.parser")
+soup = BeautifulSoup(htmlicerigi, "html.parser")
 
-basliklar=soup.findAll("td",{"class":"titleColumn"})
-ratingler=soup.findAll("td",{"class":"ratingColumn imdbRating"})
+basliklar = soup.findAll("td", {"class": "titleColumn"})
+ratingler = soup.findAll("td", {"class": "ratingColumn imdbRating"})
 
 while True:
-    altdeger=float(input("alt rating girin (0-10) :"))
-    ustdeger=float(input("alt degerin üzerinde deger girininz ust rating girin (0-10): "))
+    altdeger = float(input("Alt rating girin (0-10): "))
+    ustdeger = float(input("Alt degerin üzerinde deger girininz ust rating girin (0-10): "))
 
-    if(altdeger<ustdeger):
+    if altdeger < ustdeger:
         for baslik, rating in zip(basliklar, ratingler):
-            baslik = baslik.text
-            baslik = baslik.strip()
-            baslik = baslik.replace("\n", "")
-            rating = rating.text
-            rating = rating.strip()
-            rating = rating.replace("\n", "")
+            baslik = baslik.text.strip().replace("\n", "")
+            rating = rating.text.strip().replace("\n", "")
 
-            if (float(rating) > altdeger and float(rating) < ustdeger):
-                print("film ismi: {} film rating: {}".format(baslik, rating))
+            if float(rating) > altdeger and float(rating) < ustdeger:
+                print("Film ismi: {} | Film rating: {}".format(baslik, rating))
         print("\n")
     else:
         print("Tekrar deneyin")
-
-
-
-
-
-
