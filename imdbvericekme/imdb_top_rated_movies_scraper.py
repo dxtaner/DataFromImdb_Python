db5c3b6d3faf86b9aaea3023cb9f58b52f5b4b20
Taner Özer <tanerozer16@gmail.com>
2023-10-30 15:42:34 +0300

    Update imdb_top_rated_movies_scraper.py

--- imdbvericekme/imdb_top_rated_movies_scraper.py
+++ imdbvericekme/imdb_top_rated_movies_scraper.py
@@ -1,1 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
 
+url = "https://www.imdb.com/chart/top"
+
+response = requests.get(url)
+
+htmlicerigi = response.content
+
+soup = BeautifulSoup(htmlicerigi, "html.parser")
+
+basliklar = soup.findAll("td", {"class": "titleColumn"})
+ratingler = soup.findAll("td", {"class": "ratingColumn imdbRating"})
+
+while True:
+    altdeger = float(input("Alt rating girin (0-10): "))
+    ustdeger = float(input("Alt degerin üzerinde deger girininz ust rating girin (0-10): "))
+
+    if altdeger < ustdeger:
+        for baslik, rating in zip(basliklar, ratingler):
+            baslik = baslik.text.strip().replace("\n", "")
+            rating = rating.text.strip().replace("\n", "")
+
+            if float(rating) > altdeger and float(rating) < ustdeger:
+                print("Film ismi: {} | Film rating: {}".format(baslik, rating))
+        print("\n")
+    else:
+        print("Tekrar deneyin")
